857c806349138b13cd814e64863819b8dd212251
jianguo <zjg555543@163.com>
2023-09-06 19:48:23 +0800

    update

---
+++ start.sh
@@ -0,0 +1,6 @@
+docker-compose up -d zkevm-state-db
+docker-compose up -d zkevm-pool-db
+docker-compose up -d zkevm-event-db
+docker-compose up -d zkevm-bridge-db
+docker-compose up -d zkevm-mock-l1-network
+sleep 3
