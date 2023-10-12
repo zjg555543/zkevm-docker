3a2849cf529f7e0a8c7f6a55e0922583ffafd8b1
jianguo <zjg555543@163.com>
2023-10-12 18:12:20 +0800

    update

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-docker-compose up -d zkevm-state-db
-docker-compose up -d zkevm-pool-db
-docker-compose up -d zkevm-event-db
-docker-compose up -d zkevm-bridge-db
-docker-compose up -d zkevm-mock-l1-network
+docker-compose up -d xagon-state-db
+docker-compose up -d xagon-pool-db
+docker-compose up -d xagon-event-db
+docker-compose up -d xagon-bridge-db
+docker-compose up -d xagon-mock-l1-network
 sleep 3
