7fe58545b6e2c864b10749b2607cea3cfd53d3f4
JianGuo <zjg555543@163.com>
2024-03-03 22:20:39 +0800

    update

--- 5.stop_fork6.py
+++ 5.stop_fork6.py
@@ -30,17 +30,17 @@ if __name__ == '__main__':
     docker-compose stop x1-aggregator-fork6
     docker-compose stop x1-json-rpc-fork6
 
-    docker rm x1-data-availability-fork6
-    docker rm x1-executor-fork6
-    docker rm x1-prover-fork6
-    docker rm x1-sync-fork6
+    docker rm x1-data-availability
+    docker rm x1-executor
+    docker rm x1-prover
+    docker rm x1-sync
 
-    docker rm x1-eth-tx-manager-fork6
-    docker rm x1-sequencer-fork6
-    docker rm x1-sequence-sender-fork6
-    docker rm x1-l2gaspricer-fork6
-    docker rm x1-aggregator-fork6
-    docker rm x1-json-rpc-fork6
+    docker rm x1-eth-tx-manager
+    docker rm x1-sequencer
+    docker rm x1-sequence-sender
+    docker rm x1-l2gaspricer
+    docker rm x1-aggregator
+    docker rm x1-json-rpc
 
     '''
     result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, text=True)
