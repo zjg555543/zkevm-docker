cfe36a13b1ef5c4a459cf8bb99f8ce8cb9646b9d
JianGuo <zjg555543@163.com>
2024-03-04 00:02:21 +0800

    update

--- 3.upgrade_fork7.py
+++ 3.upgrade_fork7.py
@@ -71,7 +71,7 @@ if __name__ == '__main__':
     replace_file('./fork7/zkevm-contracts/upgrade/upgradeToV2/deploy_parameters.json', 'consensusContract', "PolygonZkEVMEtrog")
     replace_file('./fork7/zkevm-contracts/upgrade/upgradeToV2/deploy_output.json', 'gasTokenAddress', OKBAddress)
     replace_file('./fork7/zkevm-contracts/upgrade/upgradeToV2/upgrade_parameters.json', 'realVerifier', True)
-    replace_file('./fork7/zkevm-contracts/upgrade/upgradeToV2/upgrade_parameters.json', 'timelockDelay', 360)
+    replace_file('./fork7/zkevm-contracts/upgrade/upgradeToV2/upgrade_parameters.json', 'timelockDelay', 125)
     replace_file('./fork7/zkevm-contracts/upgrade/upgradeToV2/upgrade_parameters.json', 'timelockSalt', "0x0000000000000000000000000000000000000000000000000000000000000000")
     replace_file('./fork7/zkevm-contracts/upgrade/upgradeToV2/upgrade_parameters.json', 'polTokenAddress', OKBAddress)
 
