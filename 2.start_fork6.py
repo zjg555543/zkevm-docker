6519127a640ffdab4436f4e59cddf6f74352a386
JianGuo <zjg555543@163.com>
2024-03-04 11:22:29 +0800

    update

--- 2.start_fork6.py
+++ 2.start_fork6.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
     account = loadAccount()
     genAccount = account["address"]
     genPriveKey = account["private_key"]
-    command = "cast send --legacy --from {genAccount} --private-key {genPriveKey} --rpc-url http://127.0.0.1:8123 0xC949254d682D8c9ad5682521675b8F43b102aec4 --value 0.0001ether"
+    command = "sleep 30; cast send --legacy --from {genAccount} --private-key {genPriveKey} --rpc-url http://127.0.0.1:8123 0xC949254d682D8c9ad5682521675b8F43b102aec4 --value 0.0001ether"
 
     command = command.replace("{genAccount}", account["address"])
     command = command.replace("{genPriveKey}", account["private_key"])
