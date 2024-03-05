83668b1a6b5c4f29a21e37bb6ad7adf9aa6baa68
JianGuo <zjg555543@163.com>
2024-03-05 18:15:51 +0800

    update

--- 1.deploy_fork8.py
+++ 1.deploy_fork8.py
@@ -64,25 +64,26 @@ if __name__ == '__main__':
     cd fork8; 
     git clone -b zjg/v5.0.0-rc.2-fork.8 https://github.com/okx/x1-contracts.git; 
     cd ./x1-contracts; 
-    cp ../../config/deployment/deploy_parameters.json deployment/deploy_parameters.json;  
     cp ../../config/deployment/.env .env;  
-    cp ../../config/deployment/hardhat.config.js hardhat.config.js;  
-    cp ../../config/deployment/1_createGenesis.js deployment/1_createGenesis.js
+    cp ../../config/deployment/1_createGenesis.ts deployment/v2/1_createGenesis.ts
+    cp ../../config/deployment/create_rollup_parameters.json deployment/v2/create_rollup_parameters.json
+    cp ../../config/deployment/deploy_parameters.json deployment/v2/deploy_parameters.json;  
     '''
     result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, text=True)
     logging.info(result.stdout)
     replace_variable('./fork8/x1-contracts/.env', '{MNEMONIC}', genMnemonic)
-    replace_variable('./fork8/x1-contracts/deployment/deploy_parameters.json', '{ADMIN}', genAccount)
+    replace_variable('./fork8/x1-contracts/v2/deployment/create_rollup_parameters.json', '{ADMIN}', genAccount)
+    replace_variable('./fork8/x1-contracts/v2/deployment/deploy_parameters.json', '{ADMIN}', genAccount)
 
     # 部署合约
     command = '''
     cd ./fork8/x1-contracts; 
     npm i; 
-    npm run deploy:deployer:ZkEVM:sepolia; 
-    npm run  deploy:ZkEVM:sepolia; 
-    npm run  verify:ZkEVM:sepolia; 
-    cat deployment/genesis.json; 
-    cat deployment/deploy_output.json 
+    npm run deploy:testnet:v2:sepolia; 
+    npm run  verify:v2:sepolia; 
+    cat deployment/v2/create_rollup_output.json;
+    cat deployment/v2/deploy_output.json;
+    cat deployment/v2/genesis.json;
     '''
     result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, text=True)
     logging.info(result.stdout)
@@ -90,7 +91,7 @@ if __name__ == '__main__':
     # 编译node
     command = '''
     cd fork8; 
-    git clone -b release/v0.2.0 https://github.com/okx/x1-node.git; 
+    git clone -b zjg/fork8-upgrade https://github.com/okx/x1-node.git; 
     cd x1-node; 
     docker build -t x1-node-fork8 -f ./Dockerfile .
     '''
@@ -100,7 +101,7 @@ if __name__ == '__main__':
     # 编译da
     command = '''
     cd fork8;
-    git clone -b release/v0.2.0 https://github.com/okx/x1-data-availability.git; 
+    git clone -b zjg/fork8-upgrade https://github.com/okx/x1-data-availability.git; 
     cd x1-data-availability; 
     docker build -t x1-data-availability-fork8 -f ./Dockerfile .
     '''
@@ -110,7 +111,7 @@ if __name__ == '__main__':
     # 编译bridge
     command = ''' 
     cd fork8;
-    git clone -b release/v0.2.0 https://github.com/okx/x1-bridge-service.git;
+    git clone -b hai/fork8 https://github.com/okx/x1-bridge-service.git;
     cd x1-bridge-service;
     docker build -t x1-bridge-service-fork8 -f ./Dockerfile .
     '''
@@ -119,12 +120,14 @@ if __name__ == '__main__':
     logging.info(result.stdout)
 
     # 替换文件
-    polygonZkEVMAddress = get_value('./fork8/x1-contracts/deployment/deploy_output.json', 'polygonZkEVMAddress')
-    polygonZkEVMGlobalExitRootAddress = get_value('./fork8/x1-contracts/deployment/deploy_output.json', 'polygonZkEVMGlobalExitRootAddress')
-    dataCommitteeContract = get_value('./fork8/x1-contracts/deployment/deploy_output.json', 'dataCommitteeContract')
-    deploymentBlockNumber = get_value('./fork8/x1-contracts/deployment/deploy_output.json', 'deploymentBlockNumber')
-    polygonZkEVMBridgeAddress = get_value('./fork8/x1-contracts/deployment/deploy_output.json', 'polygonZkEVMBridgeAddress')
-    genesisStr = get_genesis('./fork8/x1-contracts/deployment/genesis.json')
+    dataCommitteeContract = get_value('./fork8/x1-contracts/deployment/v2/create_rollup_output.json', 'polygonDataCommittee')
+    deploymentBlockNumber = get_value('./fork8/x1-contracts/deployment/v2/create_rollup_output.json', 'createRollupBlock')
+    rollupAddress = get_value('./fork8/x1-contracts/deployment/v2/create_rollup_output.json', 'rollupAddress')
+
+    polygonZkEVMAddress = get_value('./fork8/x1-contracts/deployment/v2/deploy_output.json', 'polygonRollupManager')
+    polygonZkEVMGlobalExitRootAddress = get_value('./fork8/x1-contracts/deployment/v2/deploy_output.json', 'polygonZkEVMGlobalExitRootAddress')
+    polygonZkEVMBridgeAddress = get_value('./fork8/x1-contracts/deployment/v2/deploy_output.json', 'polygonZkEVMBridgeAddress')
+    genesisStr = get_genesis('./fork8/x1-contracts/deployment/v2/genesis.json')
 
     replace_variable('./config/fork8/test.da.toml', '{ZkEVMAddress}', polygonZkEVMAddress)
     replace_variable('./config/fork8/test.da.toml', '{DataCommitteeAddress}', dataCommitteeContract)
@@ -139,9 +142,9 @@ if __name__ == '__main__':
     replace_variable('./config/fork8/config.bridge.toml', '{PolygonBridgeAddress}', polygonZkEVMBridgeAddress)
     replace_variable('./config/fork8/config.bridge.toml', '{PolygonZkEVMGlobalExitRootAddress}', polygonZkEVMGlobalExitRootAddress)
 
-    replace_variable('./docker-compose.yml', '{ETHEREUM_BRIDGE_CONTRACT_ADDRESS_FORK6}', polygonZkEVMBridgeAddress)
-    replace_variable('./docker-compose.yml', '{ETHEREUM_PROOF_OF_EFFICIENCY_CONTRACT_ADDRESS_FORK6}', polygonZkEVMAddress)
-    replace_variable('./docker-compose.yml', '{POLYGON_ZK_EVM_BRIDGE_CONTRACT_ADDRESS_FORK6}', polygonZkEVMBridgeAddress)
+    replace_variable('./docker-compose.yml', '{ETHEREUM_BRIDGE_CONTRACT_ADDRESS_FORK8}', polygonZkEVMBridgeAddress)
+    replace_variable('./docker-compose.yml', '{ETHEREUM_PROOF_OF_EFFICIENCY_CONTRACT_ADDRESS_FORK8}', polygonZkEVMAddress)
+    replace_variable('./docker-compose.yml', '{POLYGON_ZK_EVM_BRIDGE_CONTRACT_ADDRESS_FORK8}', polygonZkEVMBridgeAddress)
 
     # 设置da地址
     command = "cast send --legacy --from {genAccount} --private-key {genPriveKey} --rpc-url https://rpc.ankr.com/eth_sepolia/578c95407e7831f0ac1ef79cacae294dc9bf8307121ca9fffaf1e556a5cca662 {dataCommitteeContract} 'function setupCommittee(uint256 _requiredAmountOfSignatures, string[] urls, bytes addrsBytes) returns()' 1 [http://x1-data-availability:8444] 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
