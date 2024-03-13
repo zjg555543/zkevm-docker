46f1bd78da4ae0a98b6200152d7e5a6e1a419868
JianGuo <zjg555543@163.com>
2024-03-13 14:53:11 +0800

    update

--- 1.deploy_fork6.py
+++ 1.deploy_fork6.py
@@ -121,3 +121,48 @@ if __name__ == '__main__':
     logging.info("docker-compose logs --tail 50 -f | grep x1-sequencer")
     logging.info("Deploy fork6 done.")
 
+    print('Deploying fork6...')
+
+    command = '''
+    docker-compose up -d x1-state-db
+    docker-compose up -d x1-pool-db
+    docker-compose up -d x1-event-db
+    docker-compose up -d x1-data-availability-db
+    docker-compose up -d x1-bridge-db
+    docker-compose up -d x1-bridge-redis
+
+    sleep 3
+    docker-compose up -d x1-data-availability-fork6
+
+    sleep 3
+    docker-compose up -d x1-executor-fork6
+    sleep 3
+    docker-compose up -d x1-prover-fork6
+    sleep 3
+    docker-compose up -d x1-sync-fork6
+    sleep 3
+    docker-compose up -d x1-eth-tx-manager-fork6
+    sleep 3
+    docker-compose up -d x1-sequencer-fork6
+    sleep 3
+    docker-compose up -d x1-sequence-sender-fork6
+    sleep 3
+    docker-compose up -d x1-l2gaspricer-fork6
+    sleep 3
+    docker-compose up -d x1-aggregator-fork6
+    sleep 3
+    docker-compose up -d x1-json-rpc-fork6
+    sleep 3
+    docker-compose up -d kafka-zookeeper
+    sleep 3
+    docker-compose up -d x1-bridge-coin-kafka
+    sleep 3
+    docker-compose up -d x1-bridge-service-fork6
+    sleep 3
+    docker-compose up -d x1-bridge-ui-fork6
+
+    '''
+    result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, text=True)
+    logging.info(result.stdout)
+    logging.info("docker-compose logs --tail 10 -f")
+
