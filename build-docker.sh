a9032e6c74f3a47f1dafd5493e480da5d9f26e35
JianGuo <zjg555543@163.com>
2024-07-10 12:54:33 +0800

    update

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,7 @@
+# docker stop $(docker ps -aq); docker rm $(docker ps -aq);docker ps -a
+# docker rmi --force $(docker images -q)
+
 docker build -t zkevm-aggregator -f ./Dockerfile.aggregator .
 docker build -t zkevm-seqsender -f ./Dockerfile.seqsender .
+
+cd xlayer-erigon; make build-docker
