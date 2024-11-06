f8d9fbca191c1daee964eb4e4c6ddef678afa0c5
jianguo <zjg555543@163.com>
2024-11-06 18:30:07 +0800

    update

--- 2.config.py
+++ 2.config.py
@@ -79,6 +79,7 @@ if __name__ == '__main__':
     print('Config ...')
 
     # 获取变量
+    adminAccount = loadAccount()["address"]
     rawDeplymentBlockNumber = get_value('./fork13/xlayer-contracts/deployment/v2/create_rollup_output.json', 'createRollupBlockNumber')
     deploymentBlockNumber = str(int(rawDeplymentBlockNumber) - 1)
     polygonZkEVMAddress = get_value('./fork13/xlayer-contracts/deployment/v2/create_rollup_output.json', 'rollupAddress')
@@ -96,24 +97,27 @@ if __name__ == '__main__':
     shutil.copy('./config/template/dynamic-mynetwork-chainspec.json', './config/erigon')
     shutil.copy('./config/template/dynamic-mynetwork-conf.json', './config/erigon')
 
-    shutil.copy('./config/template/aggregator.node.config.toml', './config/erigon')
-    shutil.copy('./config/template/seqsender.node.config.toml', './config/erigon')
+    # shutil.copy('./config/template/aggregator.node.config.toml', './config/erigon')
+    # shutil.copy('./config/template/seqsender.node.config.toml', './config/erigon')
     shutil.copy('./config/template/test.erigon.seq.config.yaml', './config/erigon')
     shutil.copy('./config/template/test.genesis.config.json', './config/erigon')
     shutil.copy('./config/template/test.node.config.toml', './config/erigon')
     shutil.copy('./config/template/test.prover.config.json', './config/erigon')
     shutil.copy('./config/template/test.stateless_executor.config.json', './config/erigon')
+    shutil.copy('./config/template/cdk.config.toml', './config/erigon')
     
     file_list = [
         "./config/erigon/test.genesis.config.json", 
-        "./config/erigon/aggregator.node.config.toml", 
-        "./config/erigon/seqsender.node.config.toml", 
+        # "./config/erigon/aggregator.node.config.toml", 
+        # "./config/erigon/seqsender.node.config.toml", 
+        "./config/erigon/cdk.config.toml", 
         "./config/erigon/test.erigon.seq.config.yaml",
         "./config/erigon/dynamic-mynetwork-conf.json",
         "./config/erigon/dynamic-mynetwork-allocs.json"
     ]
 
     for file in file_list:
+        replace_variable(file, '{adminAccount}', adminAccount)
         replace_variable(file, '{polygonZkEVMAddress}', polygonZkEVMAddress)
         replace_variable(file, '{polygonRollupManagerAddress}', polygonRollupManagerAddress)
         replace_variable(file, '{polygonZkEVMGlobalExitRootAddress}', polygonZkEVMGlobalExitRootAddress)
